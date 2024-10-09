327813fa2031c10b7e21bc1834b2099628d8234b
Ddoongssi <yzervincent@gmail.com>
2024-10-09 23:12:31 +0800

    Adding of Flowbite and Configs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,8 @@
 <script lang="ts">import "../app.css";</script><slot></slot>
+
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
+  </svelte:head>
